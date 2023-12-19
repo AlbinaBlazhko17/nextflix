9f3305051e12fea8cb04713ff76705e7d6f9543d
Albinator <albinamivina@gmail.com>
2023-12-19 21:14:45 +0200

    chore: init project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({ weight: ['400'], subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<main className={roboto.className}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
 
-export default MyApp
+export default MyApp;
