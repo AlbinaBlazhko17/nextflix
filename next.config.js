b49992b08cba8b3cbeff8f6e0bc3e8d4878094f8
Albinator <albinamivina@gmail.com>
2023-12-20 17:41:30 +0200

    chore: set up prettier, eslint and aliases

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
+//@eslint-ignore
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+};
 
-module.exports = nextConfig
+module.exports = {
+	...nextConfig,
+	webpack: (config, { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+			'@': path.resolve(__dirname),
+			'@/components': path.resolve(__dirname, 'components'),
+			'@/public': path.resolve(__dirname, 'public'),
+		};
+		return config;
+	},
+};
