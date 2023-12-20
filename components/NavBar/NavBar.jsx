d4afd16169cd9235b8ea5be0ee74ce76cb3ef01b
Albinator <albinamivina@gmail.com>
2023-12-20 19:27:22 +0200

    feat: add navbar and styles to it

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -2,21 +2,33 @@ import styles from './NavBar.module.scss';
 
 function NavBar({ username }) {
 	return (
-		<div>
-			<h2>Hello from the navbar</h2>
-			<ul>
-				<li>Home</li>
-				<li>My List</li>
-			</ul>
-			<nav>
-				<div>
-					<p>{username}</p>
-					<button></button>
-					<div>
-						<a href="#">Sign out</a>
-					</div>
+		<div className={styles.navbar}>
+			<div className={styles.navbar__wrapper}>
+				<div className={styles.navbar__logo}>
+					<a href="/" className={styles.navbar__logoLink}>
+						Nextflix
+					</a>
 				</div>
-			</nav>
+				<ul className={styles.navbar__list}>
+					<li className={styles['navbar__list-item']}>Home</li>
+					<li className={styles['navbar__list-item']}>My List</li>
+				</ul>
+				<nav className={styles.navbar__nav}>
+					<div>
+						<button className={styles.navbar__userBtn}>
+							<p className={styles.navbar__username}>{username}</p>
+						</button>
+						<div className={styles.navbar__dropdown}>
+							<div>
+								<a href="#" className={styles.navbar__sign}>
+									Sign out
+								</a>
+								<div className={styles.navbar__line}></div>
+							</div>
+						</div>
+					</div>
+				</nav>
+			</div>
 		</div>
 	);
 }
