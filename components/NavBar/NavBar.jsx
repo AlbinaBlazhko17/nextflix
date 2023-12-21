aea7209998781b212af775daf16d5aa95b89ea05
Albinator <albinamivina@gmail.com>
2023-12-21 15:59:59 +0200

    feat: change some styles and add icons

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { useState } from 'react';
+import ExpandMoreIcon from '@/public/static/expand_more.svg';
+import Image from 'next/image';
+import { motion } from 'framer-motion';
 
 import styles from './NavBar.module.scss';
 
@@ -27,32 +30,56 @@ function NavBar({ username }) {
 		<div className={styles.navbar}>
 			<div className={styles.navbar__wrapper}>
 				<div className={styles.navbar__logo}>
-					<a href="/" className={styles.navbar__logoLink}>
+					<a
+						href="/"
+						className={styles.navbar__logoLink}
+					>
 						Nextflix
 					</a>
 				</div>
 				<ul className={styles.navbar__list}>
-					<li className={styles['navbar__list-item']} onClick={handleOnClickHome}>
+					<li
+						className={styles['navbar__list-item']}
+						onClick={handleOnClickHome}
+					>
 						Home
 					</li>
-					<li className={styles['navbar__list-item']} onClick={handleOnClickMyList}>
+					<li
+						className={styles['navbar__list-item']}
+						onClick={handleOnClickMyList}
+					>
 						My List
 					</li>
 				</ul>
 				<nav className={styles.navbar__nav}>
 					<div>
-						<button className={styles.navbar__userBtn} onClick={handleToogleDropdown}>
+						<button
+							className={styles.navbar__userBtn}
+							onClick={handleToogleDropdown}
+						>
 							<p className={styles.navbar__username}>{username}</p>
+							<div className={styles.navbar__expand}>
+								<Image
+									src={ExpandMoreIcon.src}
+									width={20}
+									height={20}
+									alt="expand more"
+								/>
+							</div>
 						</button>
 						{showDropdown && (
-							<div className={styles.navbar__dropdown}>
-								<div>
-									<Link href="/login">
-										<a className={styles.navbar__sign}>Sign out</a>
-									</Link>
-									<div className={styles.navbar__line}></div>
-								</div>
-							</div>
+							<motion.div
+								initial={{ marginTop: '-1%', opacity: 0 }}
+								animate={{ marginTop: '0.5rem', opacity: 1 }}
+								exit={{ marginTop: '-1%', opacity: 0 }}
+								transition={{ duration: 0.2 }}
+								className={styles.navbar__dropdown}
+							>
+								<Link href="/login">
+									<a className={styles.navbar__sign}>Sign out</a>
+								</Link>
+								<div className={styles.navbar__line}></div>
+							</motion.div>
 						)}
 					</div>
 				</nav>
