3d147fc34572cec974bc965e2be6c011be0376f1
Albinator <albinamivina@gmail.com>
2023-12-21 10:51:20 +0200

    feat: add routing

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,6 +1,21 @@
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+
 import styles from './NavBar.module.scss';
 
 function NavBar({ username }) {
+	const router = useRouter();
+
+	function handleOnClickHome(e) {
+		e.preventDefault();
+		router.push('/');
+	}
+
+	function handleOnClickMyList(e) {
+		e.preventDefault();
+		router.push('/my-list');
+	}
+
 	return (
 		<div className={styles.navbar}>
 			<div className={styles.navbar__wrapper}>
@@ -10,8 +25,12 @@ function NavBar({ username }) {
 					</a>
 				</div>
 				<ul className={styles.navbar__list}>
-					<li className={styles['navbar__list-item']}>Home</li>
-					<li className={styles['navbar__list-item']}>My List</li>
+					<li className={styles['navbar__list-item']} onClick={handleOnClickHome}>
+						Home
+					</li>
+					<li className={styles['navbar__list-item']} onClick={handleOnClickMyList}>
+						My List
+					</li>
 				</ul>
 				<nav className={styles.navbar__nav}>
 					<div>
@@ -20,9 +39,9 @@ function NavBar({ username }) {
 						</button>
 						<div className={styles.navbar__dropdown}>
 							<div>
-								<a href="#" className={styles.navbar__sign}>
-									Sign out
-								</a>
+								<Link href="/login">
+									<a className={styles.navbar__sign}>Sign out</a>
+								</Link>
 								<div className={styles.navbar__line}></div>
 							</div>
 						</div>
