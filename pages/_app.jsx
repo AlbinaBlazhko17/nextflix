fdd745097d63922621842198250b822b7fff3117
Albinator <albinamivina@gmail.com>
2023-12-21 10:51:35 +0200

    refactor: fonts in next 12

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,7 @@
 import '@/styles/globals.scss';
-import { Roboto_Slab } from 'next/font/google';
-
-const roboto = Roboto_Slab({ weight: ['400', '600', '700', '800'], subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<div className={roboto.className}>
-			<Component {...pageProps} />
-		</div>
-	);
+	return <Component {...pageProps} />;
 }
 
 export default MyApp;
