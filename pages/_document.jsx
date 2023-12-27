7916f1e4c05e760478bcad34a7250b3e79f61cc1
Albinator <albinamivina@gmail.com>
2023-12-27 10:48:40 +0200

    chore: add styles and prettier code

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,8 +5,15 @@ class MyDocument extends Document {
 		return (
 			<Html>
 				<Head>
-					<link rel="preconnect" href="https://fonts.googleapis.com" />
-					<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
 					<link
 						href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300;400;500;600;700;800&display=swap"
 						rel="stylesheet"
