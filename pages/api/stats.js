2ca6fd4976e048e8039b638052e6ba5722d798a3
Albinator <albinamivina@gmail.com>
2023-12-28 17:40:52 +0200

    feat: handle get request in stats endpoint

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -2,39 +2,49 @@ import jwt from 'jsonwebtoken';
 import { findVideoIdByUser, createNewStats, updateStats } from '@/lib/db/hasura';
 
 export default async function stats(req, res) {
-	if (req.method === 'POST') {
-		try {
-			const token = req.cookies.token;
-			if (!token) {
-				res.status(403).send('Forbidden');
-			} else {
-				const decodedToken = jwt.verify(token, process.env.NEXT_PUBLIC_SECRET_JWT_TOKEN);
-				const userId = decodedToken.issuer;
-				const { videoId, favorited, watched = true } = req.body;
-				const findVideById = await findVideoIdByUser(token, userId, videoId);
-				if (findVideById) {
-					const updatedStats = await updateStats(token, {
-						favorited,
-						watched,
-						userId,
-						videoId,
-					});
-					res.status(200).send({
-						msg: 'Post request to stats',
-						updatedStats,
-					});
-				} else {
-					const newStats = await createNewStats(token, {
-						favorited: 0,
-						watched: false,
-						videoId,
-						userId: userId,
-					});
-					res.status(200).send({ msg: 'Post request to stats', newStats });
-				}
-			}
-		} catch (err) {
-			res.status(500).send('Error! ' + err);
+	try {
+		const token = req.cookies.token;
+		const decodedToken = jwt.verify(token, process.env.NEXT_PUBLIC_SECRET_JWT_TOKEN);
+		const userId = decodedToken.issuer;
+		const { videoId } = req.method === 'POST' ? req.body : req.query;
+		const findVideById = await findVideoIdByUser(token, userId, videoId);
+		const isStatsExist = findVideById?.length > 0;
+		if (!token) {
+			res.status(403).send('Forbidden');
 		}
+
+		if (req.method === 'GET') {
+			if (isStatsExist) {
+				res.status(200).send({ msg: 'Get request to stats', findVideById });
+			}
+			res.status(404).send({ msg: 'Video not found' });
+		}
+
+		if (req.method === 'POST') {
+			const { favorited, watched } = req.body;
+
+			if (isStatsExist) {
+				const updatedStats = await updateStats(token, {
+					favorited,
+					watched,
+					userId,
+					videoId,
+				});
+				res.status(200).send({
+					msg: 'Post request to stats',
+					updatedStats,
+				});
+			} else {
+				const newStats = await createNewStats(token, {
+					favorited: 0,
+					watched: false,
+					videoId,
+					userId: userId,
+				});
+				res.status(200).send({ msg: 'Post request to stats', newStats });
+			}
+		}
+	} catch (err) {
+		res.status(500).send('Error! ' + err);
 	}
 }
